ee50280d7092a6de152f07dd8da32ba57c42a79b
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-03-15 12:10:55 -0700

    0x120x13javascripttasks

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const number = parseInt(process.argv[2]);
-function factorial (num) {
-  if (isNaN(num) || num === 0) return (1);
-  return (num * factorial(num - 1));
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-console.log(factorial(number));
+
+console.log(factorial(Number(process.argv[2])));
